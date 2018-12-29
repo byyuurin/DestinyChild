4918668b97488e9270b39b56937eb8ae37118632
yuurin <byyuurin@gmail.com>
2018-12-29 13:44:04 +0800

    add component: SkillInformation

---
+++ src/components/SkillInformation/index.vue
@@ -0,0 +1,44 @@
+<style lang="scss" src="./style.scss" scoped></style>
+<template src="./template.html"></template>
+<script>
+import Portrait from '@/components/Portrait'
+export default {
+  name: 'skill-information',
+  props: {
+    it: {
+      type: Object,
+      required: true,
+      default: {
+        id: 0,
+        nickname: '',
+        name_CH: '',
+        name_JP: '',
+        name_KR: '',
+        avatar: '',
+        star: '',
+        type: '',
+        profession: '',
+        recommended: '0',
+        cooldown: '',
+        LeaderSkill: '',
+        BaseSkill: '',
+        NormalSkill: '',
+        SlideSkill: '',
+        DriveSkill: '',
+        createDate: '',
+        createUser: null
+      }
+    }
+  },
+  components: {
+    Portrait
+  },
+  methods: {
+    numberHighlight(text) {
+      return text.replace(/[(\+|\-)?\d.?\d]+/gi, mathText => {
+        return `<span class="highlight">${mathText}</span>`
+      })
+    }
+  }
+}
+</script>
