5b56c4d8e83978aa4bf3fbaf95a3552e2c369d38
yuurin <byyuurin@gmail.com>
2018-12-29 13:42:57 +0800

    add component: Portrait

---
+++ src/components/Portrait/index.vue
@@ -0,0 +1,38 @@
+<style lang="scss" src="./style.scss" scoped></style>
+<template src="./template.html"></template>
+<script>
+export default {
+  name: 'portrait',
+  props: {
+    it: {
+      type: Object,
+      required: true,
+      default: {
+        id: 0,
+        nickname: '',
+        name_CH: '',
+        name_JP: '',
+        name_KR: '',
+        avatar: '',
+        star: '',
+        type: '',
+        profession: '',
+        recommended: '0',
+        cooldown: '',
+        LeaderSkill: '',
+        BaseSkill: '',
+        NormalSkill: '',
+        SlideSkill: '',
+        DriveSkill: '',
+        createDate: '',
+        createUser: null
+      }
+    },
+    hasHoverEffect: {
+      type: Boolean,
+      required: false,
+      default: true
+    }
+  }
+}
+</script>
