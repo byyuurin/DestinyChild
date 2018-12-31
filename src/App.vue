1308926a0a214387e25413f0cf67d2111ad7cddf
yuurin <byyuurin@gmail.com>
2019-01-01 00:45:17 +0800

    remove test code.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="app">
     <global-header/>
-    <div class="main">
-      <router-view/>
-    </div>
+    <router-view/>
     <global-footer/>
   </div>
 </template>
@@ -11,23 +9,9 @@
 import GlobalHeader from '@/components/GlobalHeader'
 import GlobalFooter from '@/components/GlobalFooter'
 export default {
-  data() {
-    return {
-      db: []
-    }
-  },
   components: {
     GlobalHeader,
     GlobalFooter
-  },
-  mounted() {
-    // fetch('https://dc.u030y.lionfree.net/ChildList/api/portrait')
-    //   .then(res => {
-    //     return res.json()
-    //   })
-    //   .then(data => {
-    //     this.db = data
-    //   })
   }
 }
 </script>
