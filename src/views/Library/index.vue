e3937aae42d7ac72e7e2bdd6c4471aaa9dd329c7
yuurin <byyuurin@gmail.com>
2019-01-01 22:26:25 +0800

    add search filter.

--- src/views/Library/index.vue
+++ src/views/Library/index.vue
@@ -10,18 +10,43 @@ export default {
   data() {
     return {
       character: null,
+      openFilter: false,
+      showUnknown: false,
       groupSize: {
         'child-5': 108,
         'child-4': 64,
         'child-3': 97,
         'child-2': 53,
         'child-1': 44
-      }
+      },
+      types: ['fire', 'water', 'forest', 'light', 'dark'],
+      professions: ['attack', 'protect', 'support', 'confusion', 'medic'],
+      filter: {}
     }
   },
   computed: {
+    filteredCharacters() {
+      return this.$store.state.characters.filter(it => {
+        let { name, type, profession } = this.filter
+        let isFind = true
+
+        if (name) {
+          isFind = it.nickname.indexOf(name) >= 0
+          isFind = isFind || it.name_CH.indexOf(name) >= 0
+        }
+
+        if (it.type) {
+          isFind = isFind && type[it.type]
+        }
+        if (it.profession) {
+          isFind = isFind && profession[it.profession]
+        }
+
+        return isFind
+      })
+    },
     characters() {
-      return this.$store.state.characters
+      return this.filteredCharacters
     },
     characterMap() {
       let map = {}
@@ -65,10 +90,36 @@ export default {
       return groups
     }
   },
+  watch: {
+    openFilter() {
+      this.lockWindow(this.openFilter)
+    }
+  },
   created() {
     this.$store.dispatch('CHARACTER_READ')
+
+    this.types.forEach(it => {
+      this.filter.type = {}
+      this.types.forEach(it => {
+        this.filter.type[it] = true
+      })
+
+      this.filter.profession = {}
+      this.professions.forEach(it => {
+        this.filter.profession[it] = true
+      })
+
+      this.filter.name = ''
+    })
   },
   methods: {
+    lockWindow(isLock) {
+      if (isLock) {
+        document.body.classList.add('locked')
+      } else {
+        document.body.classList.remove('locked')
+      }
+    },
     numberToString(number, minLength) {
       let strNumbers = number.toString().split('')
       for (let i = strNumbers.length; i < minLength; i++) {
@@ -86,26 +137,58 @@ export default {
     getCollectionCounts(characters) {
       let counts = 0
       characters.forEach(character => {
-        if (character.type) {
+        if (this.showUnknown && character.type) {
+          counts++
+        } else if (!this.showUnknown && character.id) {
           counts++
         }
       })
       return counts
     },
+    filterClone() {
+      let { name, type, profession } = this.filter
+      let filter = {}
+
+      filter.name = name
+
+      this.types.forEach(it => {
+        filter.type = {}
+        this.types.forEach(it => {
+          filter.type[it] = type[it]
+        })
+
+        filter.profession = {}
+        this.professions.forEach(it => {
+          filter.profession[it] = profession[it]
+        })
+      })
+      return filter
+    },
+    filterHandler(key, target) {
+      let filter = this.filterClone()
+
+      if (key === 'name') {
+        filter[key] = target.value
+      } else {
+        let keys = key.split('.')
+        filter[keys[0]][keys[1]] = target.checked
+      }
+      this.filter = filter
+    },
     informationHandler(avatar) {
       const character = this.characterMap[avatar] || null
       if (character) {
         this.character = character
-        document.body.classList.add('locked')
+        this.lockWindow(true)
       }
     },
     closeInformationHandler() {
       this.character = null
-      document.body.classList.remove('locked')
+      this.lockWindow(false)
     }
   },
   beforeDestroy() {
-    document.body.classList.remove('locked')
+    this.lockWindow(false)
   },
   components: {
     Portrait,
