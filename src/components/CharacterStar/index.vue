72112df9794819c8b682bd8c3cdceaabdb4172a8
yuurin <byyuurin@gmail.com>
2019-01-01 12:58:35 +0800

    add component: CharacterStar

---
+++ src/components/CharacterStar/index.vue
@@ -0,0 +1,24 @@
+<style lang="scss" src="./style.scss" scoped></style>
+<template src="./template.html"></template>
+<script>
+export default {
+  name: 'character-star',
+  props: {
+    stars: {
+      type: Number,
+      required: true,
+      default: 1
+    },
+    counts: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    total: {
+      type: Number,
+      required: true,
+      default: 0
+    }
+  }
+}
+</script>
