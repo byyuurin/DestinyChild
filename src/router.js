ecf4b05055753267d76657bacb81d7b13b4f35e7
yuurin <byyuurin@gmail.com>
2019-01-12 13:37:56 +0800

    update router

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from './views/Home/index.vue'
 
 Vue.use(Router)
 
@@ -58,7 +58,7 @@ export default new Router({
     },
     {
       path: '*',
-      redirect: { name: 'Library' }
+      redirect: { name: 'home' }
     }
   ],
   scrollBehavior() {
